dbb7e67cbe072c11b657fdb3624dff9dc6cf48fe
Patrick Mwangi <astroguypasa@gmail.com>
2017-02-04 12:05:15 +0300

    Added SSL_VERIFYPEER
    A security change to ensure all data is transported securely.

--- Equity.php
+++ Equity.php
@@ -85,6 +85,7 @@ class Equity
         }
         $curl = curl_init($url);
         curl_setopt($curl, CURLOPT_CUSTOMREQUEST, $method);
+        curl_setopt($curl, CURLOPT_SSL_VERIFYPEER, true);
         curl_setopt($curl, CURLOPT_RETURNTRANSFER, 1);
         curl_setopt($curl, CURLOPT_HEADER, 0);
         curl_setopt($curl, CURLOPT_POSTFIELDS, $data);
